ecafbc3188967747b8e6ba306b9719aa59774262
Fahim-Nizamee <nizameef@gmail.com>
2023-12-14 01:22:19 +0600

    GGS

--- src/adminPages/Ads.js
+++ src/adminPages/Ads.js
@@ -1,12 +1,162 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminNavbar from '../adminComponents/AdminNavbar'
 import Header from '../components/Header'
+import axios from 'axios';
+import 'react-toastify/dist/ReactToastify.css';
+import swal from 'sweetalert2';
 
 export default function Ads() {
+
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    time: '',
+    location: '',
+    image: '',
+    date: ''
+  });
+  const [ads, setAds] = useState([]);
+
+  function load() {
+    axios.get('http://localhost:5000/ads-data').then(response => {
+      setAds(response.data);
+      console.log(response.data)
+    });
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  const delete_ads = async (id, e) => {
+    e.preventDefault();
+    await axios.delete(`http://localhost:5000/delete-ad/${id}`).then((response) => {
+      if (response.data === 'success') {
+        swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Deleted Successfully',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        // Reload data after successful deletion
+        load();
+      }
+    });
+  };
+  
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const title = formData.title
+    const description = formData.description
+    const time = formData.time
+    const location = formData.location
+    const image = formData.image
+    const date = formData.date
+
+    const data = { title, description, time, location, image, date }
+
+    if (formData) {
+
+      await axios.post('http://localhost:5000/add-ads', data).then(response => {
+        if (response.data === 'success') {
+          swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Added Succesfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          setFormData({
+            title: '',
+            description: '',
+            time: '',
+            location: '',
+            image: '',
+            date: ''
+          });
+          load()
+        }
+      })
+    }
+  };
+
+
   return (
     <div>
-        <Header/>
-      <AdminNavbar/>
+      <Header />
+      <AdminNavbar />
+      <div className='d-flex flex-column flex-md-row my-2 container-fluid px-4'>
+        <div className='w-100 px-2'>
+          <h2 className='my-3 text-center'>Ads List</h2>
+          <div className='form-control'>
+            <div className='d-flex justify-content-between align-content-center my-2 px-1'>
+              <span className=''>Title</span>
+              <span className=''>Date</span>
+              <span>Action</span>
+            </div>
+
+            {ads.map(data => (
+              <div className='form-control d-flex justify-content-between align-content-center my-2'>
+                <span className=''>{data.title}</span>
+                <span className=''>{data.date}</span>
+                <a href='#' onClick={(e) => delete_ads(data._id, e)}><i className='fa fa-trash'></i></a>
+              </div>
+            ))}
+          </div>
+
+
+        </div>
+
+        <div className='w-100 px-2'>
+          <h2 className='my-3 text-center'>Add Advertisement</h2>
+          <div className='form-control justify-content-center'>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <div>
+                  <label className="form-label" >Title:</label>
+                  <input type="text" className="form-control" name="title" value={formData.title} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label className="form-label">Description:</label>
+                  <input className="form-control" type="text" name="description" value={formData.description} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label className="form-label">Time:</label>
+                  <input className="form-control" type="text" name="time" value={formData.time} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label className="form-label">Location:</label>
+                  <input className="form-control" type="text" name="location" value={formData.location} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label className="form-label">Image:</label>
+                  <input className="form-control" type="text"  name="image" value={formData.image} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label className="form-label">Date:</label>
+                  <input className="form-control" type="text" name="date" value={formData.date} onChange={handleChange} required />
+                </div>
+              </div>
+              <div className='text-center my-3'>
+                <button className='btn btn-outline-danger' type="submit">Submit</button>
+              </div>
+
+            </form>
+
+          </div>
+
+
+
+
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Info.css'
+import axios from 'axios';
 
 export default function Info(props) {
+
+  const [ads, setAds] = useState([]);
+  function load() {
+    axios.get('http://localhost:5000/ads-data').then(response => {
+      setAds(response.data);
+      console.log(response.data)
+    });
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
   return (
     <div className='promotional-materials'>
 
@@ -62,7 +76,38 @@ export default function Info(props) {
       </div> */}
       <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="3000">
+          {ads.map(data=>(
+            <div class="carousel-item active" data-bs-interval="3000">
+            <div class="campaign_slier_item mx-2" >
+              <div class="row">
+                <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-5">
+                  <div class="campaign_img">
+                    <img src={data.image}
+                    />
+                    <a href="https://www.hhs.gov/givingequalsliving/giveblood/why-give#:~:text=If%20you%20donate%20blood%2C%20it,them%20from%20making%20blood%20correctly" tabindex="-1">Read More</a>
+                  </div>
+                </div>
+                <div class="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12">
+                  <div class="campaign_content">
+                    <div class="meta_date">
+                      <span><i className='fa-solid fa-heart-pulse'></i>{data.date}</span>
+                    </div>
+                    <a href="campaign-details.html" tabindex="-1">
+                      <h6>Give Life: Donate Blood Today! Be a Hero</h6>
+                    </a>
+                    <p>{data.description}</p>
+                    <div class="meta_time d-flex gap-4">
+                      <span><i class="fa-regular fa-clock"></i>{data.time}</span>
+                      <span><i class="fa-solid fa-location-dot"></i>{data.location} </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          ))}
+          
+          {/* <div class="carousel-item" data-bs-interval="3000">
             <div class="campaign_slier_item mx-2" >
               <div class="row">
                 <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-5">
@@ -117,35 +162,7 @@ export default function Info(props) {
                 </div>
               </div>
             </div>
-          </div>
-          <div class="carousel-item" data-bs-interval="3000">
-            <div class="campaign_slier_item mx-2" >
-              <div class="row">
-                <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-5">
-                  <div class="campaign_img">
-                    <img src="https://innovativeartisan.com/demo/html/blad-ai/assets/images/c1.png"
-                    />
-                    <a href="https://www.hhs.gov/givingequalsliving/giveblood/why-give#:~:text=If%20you%20donate%20blood%2C%20it,them%20from%20making%20blood%20correctly" tabindex="-1">Read More</a>
-                  </div>
-                </div>
-                <div class="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12">
-                  <div class="campaign_content">
-                    <div class="meta_date">
-                      <span><i className='fa-solid fa-heart-pulse'></i> Life Saver</span>
-                    </div>
-                    <a href="campaign-details.html" tabindex="-1">
-                      <h6>Give Life: Donate Blood Today! Be a Hero</h6>
-                    </a>
-                    <p>Be a hero, donate blood! Your contribution saves lives. Join us today to make a difference through this invaluable gift. Together, let's build a healthier community!</p>
-                    <div class="meta_time d-flex gap-4">
-                      <span><i class="fa-regular fa-clock"></i> 24 hours</span>
-                      <span><i class="fa-solid fa-location-dot"></i>Bangladesh </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          </div> */}
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
